3d59a75e3b5a2604f7e4ccf9cf6b488d6eeda788
pennythoughtss <areik421@gmail.com>
2025-09-19 23:51:38 -0600

    grade time

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
+        /*
         Console.WriteLine("Hello Sandbox World!");
+
+        Console.Write("What is your first name? : ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? : ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
+*/
+
+        Console.WriteLine("What is your grade percentage? ");
+        string userInputText = Console.ReadLine();
+        int grade = int.Parse(userInputText);
+        string gradeLet = "A";
+        string gradeSign = "=";
+
+        if (grade >= 90)
+        {
+            gradeLet = "A";
+        }
+        else if (grade >= 80)
+        {
+            gradeLet = "B";
+        }
+        else if (grade >= 70)
+        {
+            gradeLet = "C";
+        }
+        else if (grade >= 60)
+        {
+            gradeLet = "D";
+        }
+        else
+        {
+            gradeLet = "F";
+        }
+
+        if (grade % 10 >= 7)
+        {
+            gradeSign = "+";
+        }
+        if (grade % 10 <= 3)
+        {
+            gradeSign = "-";
+        }
+
+        if (gradeLet == "A" && gradeSign == "+" || gradeLet == "F")
+        {
+            gradeSign = "";
+        }
+
+        Console.WriteLine($"{gradeLet}{gradeSign}");
+
+        if (grade > 70)
+        {
+            Console.WriteLine("Congrats! You pass!");
+        }
+        else
+        {
+            Console.WriteLine("lmao loser\n...\nI mean, you'll get 'em next time!");
+        }
+
+        
     }
 }
